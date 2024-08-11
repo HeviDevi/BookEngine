406a707ba8c3c2bdd381abd6b611f477341a2988
HeviDevi <devon.r.ross@gmail.com>
2024-08-11 01:56:07 -0400

    added link to render site on README

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,5 +15,10 @@ export default defineConfig({
         changeOrigin: true
       }
     }
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-botstrap', 'bootstrap/dist/css/bootstrap.min.css']
+    }
   }
 })
